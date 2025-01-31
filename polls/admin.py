9c407a1b7dea7681c8bdd08a5287311c1244e474
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-01-31 16:58:42 +0100

    tutorial 07

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+# lo pone de una manera inline
+# class ChoiceInline(admin.StackedInline): #deriva de stackedinline
+#     model = Choice
+#     extra = 1
+
+# lo pone como tablitas
+class ChoiceInline(admin.TabularInline): #deriva de TabularInline
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin): #deriva de modeladmin
+    fieldsets = [  #controlas las cosas que el usuario visualice y pueda editar, para ponerlo de manera chula y bonita
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently","numero_opcions"] # para que al ver las preguntas en la tabla veas más información, puedes poner
+    # atributos y también funciones que hemos hecho antes en la clase que lo que hace es uan subquery
+    list_filter = ["pub_date"] # para agregar un filtro
+    search_fields = ["question_text"] #para agregar un buscador
+
+    #saber cuántas hay de questions, en vez de en el model podemos hacerlo aquí -> estas funciones relentizan cuando hay muchas entradas y no son óptimas si hay muchas entradas
+    def numero_opcions(self,obj):
+        #obj -> el modelo que está iterando el questionadmin
+        return obj.choice_set.count()
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,9 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
+# para el decorator
+from django.contrib import admin
+
 # Esta clase te creará tanto las tablas de la bd como acceder a ellas
 
 class Question(models.Model):
@@ -13,6 +16,13 @@ class Question(models.Model):
     # esto es para que se printee de manera bonita el objeto en el front
     def __str__(self):
         return self.question_text
+
+    # es un decorator, que dice que es un boolean. Es una función que hace de wraper, y modificas el comportamiento de la otra función a través de ésta. El @admin es una librería
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     # añadimos un método, es decir una función, que se verá reflejada en el front
     def was_published_recently(self):
